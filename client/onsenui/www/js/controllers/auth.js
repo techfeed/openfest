512aa24ca9cf32acb22ded5e6eb3f579db918f27
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-05-09 16:16:47 +0900

    Clean up and separate controller files

--- client/onsenui/www/js/controllers/auth.js
+++ client/onsenui/www/js/controllers/auth.js
@@ -1,24 +1,4 @@
 angular
   .module('app')
-  .controller('LoginController', ['$scope', 'User', function($scope, User) {
-    $scope.login = function() {
-      .pushPage(
-        '/onsenui/www/models-index.html', {
-          animation: 'lift'
-        });
-    };
-    $scope.signup = function() {
-      .pushPage(
-        '/onsenui/www/signup.html', {
-          animation: 'lift'
-        });
-    };
-  }])
-  .controller('SignupController', ['$scope', 'User', function($scope, User) {
-    $scope.signup = function() {
-      .pushPage(
-        '/onsenui/www/models-index.html', {
-          animation: 'lift'
-        });
-    };
-  }]);
+  .controller('LoginController', ['$scope', 'User', function($scope, User) {}])
+  .controller('SignupController', ['$scope', 'User', function($scope, User) {}]);
