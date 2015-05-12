388606f03055c5e769f3cb4a41d0e041889dd57a
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-05-12 16:24:34 +0900

    Mmm, cannot resove autorization error.

--- client/www/js/controllers/Event/manage.js
+++ client/www/js/controllers/Event/manage.js
@@ -1,7 +1,15 @@
 (function() {
   var app = angular.module('app');
-  app.controller('EventManageController', ['$scope', 'Event', function($scope,
-    Event) {
-    $scope.records = Event.find();
+  app.controller('EventManageController', ['$scope', 'Event', 'FestUser', 'Ticket', '$state', function($scope,
+    Event, FestUser, Ticket, $state) {
+    $scope.listEntry = function() {
+      console.log('listEntry');
+      $scope.records = FestUser.tickets({id: $scope.currentUser.id});
+    };
+    $scope.listManageable = function() {
+      console.log('listManageable');
+      $scope.records = FestUser.events({id: $scope.currentUser.id});
+    }
+    $scope.listEntry();
   }]);
 })();
