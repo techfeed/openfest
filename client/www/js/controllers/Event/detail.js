44c2b2dfe1c6c1a5a9f2f4b9d53ec40a2707e584
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-05-12 11:35:01 +0900

    Implement the CRUD operations of event

--- client/www/js/controllers/Event/detail.js
+++ client/www/js/controllers/Event/detail.js
@@ -1,32 +1,34 @@
 (function() {
   var app = angular.module('app');
 
-  app.controller('EventDetailController', ['$scope', 'Event', '$state', function($scope,
-    Event, $state) {
+  app.controller('EventDetailController', ['$scope', 'Event', '$state', '$translate', function($scope,
+    Event, $state, $translate) {
     var recordId = $state.params.id;
     $scope.record = Event.findById({
       id: recordId
     });
 
     $scope.remove = function() {
-      ons.notification.confirm({
-        message: 'Are you sure?',
-        title: 'Alert',
-        buttonLabels: ['Yes', 'No'],
-        primaryButtonIndex: 1,
-        cancelable: true,
-        callback: function(index) {
-          if (index !== 0) {
-            return;
+      $translate('page.Event.delete.confirm').then(function(msg) {
+        ons.notification.confirm({
+          message: msg,
+          title: 'Alert',
+          buttonLabels: ['Yes', 'No'],
+          primaryButtonIndex: 1,
+          cancelable: true,
+          callback: function(index) {
+            if (index !== 0) {
+              return;
+            }
+            Event.removeById({
+                id: recordId
+              })
+              .$promise
+              .then(function() {
+                $state.go('Event.list');
+              });
           }
-          Event.removeById({
-              id: recordId
-            })
-            .$promise
-            .then(function() {
-              $state.go('Event.list');
-            });
-        }
+        });
       });
     };
   }]);
