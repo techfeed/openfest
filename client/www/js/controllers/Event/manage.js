0009c8eec2c7fb40bce016af4fb70e63650c8507
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-05-12 19:54:29 +0900

    List managed events

--- client/www/js/controllers/Event/manage.js
+++ client/www/js/controllers/Event/manage.js
@@ -3,11 +3,9 @@
   app.controller('EventManageController', ['$scope', 'Event', 'FestUser', 'Ticket', '$state', function($scope,
     Event, FestUser, Ticket, $state) {
     $scope.listEntry = function() {
-      console.log('listEntry');
       $scope.records = FestUser.tickets({id: $scope.currentUser.id});
     };
     $scope.listManageable = function() {
-      console.log('listManageable');
       $scope.records = FestUser.events({id: $scope.currentUser.id});
     }
     $scope.listEntry();
