9a4358fdc54ee844fcdf0219bbb2aec197a63565
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-05-13 16:30:41 +0900

    Implement waiting for cancel

--- client/www/js/controllers/Event/list.js
+++ client/www/js/controllers/Event/list.js
@@ -2,6 +2,13 @@
   var app = angular.module('app');
   app.controller('EventListController', ['$scope', 'Event', function($scope,
     Event) {
-    $scope.records = Event.find();
+    $scope.records = Event.find({
+      filter: {
+        where: {
+          published: true,
+          endAt: { gt: new Date() }
+        }
+      }
+    });
   }]);
 })();
--- common/models/ticket.js
+++ common/models/ticket.js
@@ -32,10 +32,6 @@ module.exports = function(Ticket) {
     });
   };
 
-  Ticket.cancel = function(id, cb) {
-
-  };
-
   Ticket.remoteMethod(
     'purchase',
     {
