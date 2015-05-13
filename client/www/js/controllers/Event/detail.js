66c16aac97a2cc7b912285d7ecaadd2b3befc909
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-05-13 11:55:25 +0900

    Show entry/cancel button only if the user is (not) entry.

--- client/www/js/controllers/Event/detail.js
+++ client/www/js/controllers/Event/detail.js
@@ -1,14 +1,29 @@
 (function() {
   var app = angular.module('app');
 
-  app.controller('EventDetailController', ['$scope', 'Event', 'Ticket', '$state', '$translate', function($scope,
-    Event, Ticket, $state, $translate) {
+  app.controller('EventDetailController', ['$scope', 'Event', 'Ticket', '$state', '$translate', '$q', function($scope,
+    Event, Ticket, $state, $translate, $q) {
+
+    var currentUser = $scope.currentUser;
+
     var recordId = $state.params.id;
     $scope.record = Event.findById({
-      id: recordId
+      id: recordId,
+      filter: {
+        include: [
+          { tickets: 'purchaser' },
+          'owner'
+        ]
+      }
     });
-    $scope.eventOwner = Event.owner({id: recordId});
-
+    $scope.record
+      .$promise
+      .then(function(event) {
+        $scope.didIEntry = event.tickets.some(function(ticket, index) {
+          return ticket.purchaser.id === currentUser.id;
+        });
+      });
+      
     $scope.remove = function() {
       $translate('page.Event.delete.confirm').then(function(msg) {
         ons.notification.confirm({
@@ -35,12 +50,16 @@
 
     $scope.entry = function() {
       var params = {};
-      params.ticketNo = Math.floor(Math.random() * 10000);
-      params.purchaserId = $scope.currentUser.id;
+      params.ticketNo = Math.floor(Math.random() * 100000);
+      params.purchaserId = currentUser.id;
       params.eventId = recordId;
       params.issuedAt = new Date();
-      console.log(params);
-      Ticket.create(params);
+      Ticket
+        .create(params)
+        .$promise
+        .then(function() {
+          $state.reload();
+        });
     };
   }]);
 })();
