954e92852a219ba3da65d44cff337e04d2d0fc49
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2015-05-13 15:39:14 +0900

    Add remote method: Ticket#purchase

--- common/models/ticket.js
+++ common/models/ticket.js
@@ -1,3 +1,50 @@
-module.exports = function(Ticket) {
+var async = require('async');
 
+module.exports = function(Ticket) {
+  Ticket.purchase = function(eventId, userId, cb) {
+    Event = Ticket.app.models.Event;
+
+    async.parallel({
+      event: function(callback) {
+        Event.findById(eventId, callback);
+      },
+      countOfTicket: function(callback) {
+        // Not cancelled
+        Ticket.count({where: {exists: {cancelledAt: false}}}, callback);
+      }
+    }, function(err, results) {
+      if (err) {
+        return cb(err);
+      }
+      var now = new Date();
+      var ticketNo = Math.floor(Math.random() * 100000);
+      var ticket = {
+        eventId: eventId,
+        purchaserId: userId,
+        ticketNo: ticketNo,
+        createdAt: now
+      };
+      // Ticket remains
+      if (results.event.amountOfTicket > results.countOfTicket) {
+        ticket.issuedAt = now;
+      }
+      Ticket.create(ticket, cb);
+    });
+  };
+
+  Ticket.cancel = function(id, cb) {
+
+  };
+
+  Ticket.remoteMethod(
+    'purchase',
+    {
+      accepts: [
+        { arg: 'eventId', type: 'string', required: true },
+        { arg: 'userId', type: 'string', required: true }
+      ],
+      http: { path: '/purchase', verb: 'post' },
+      returns: { arg: 'ticket', type: 'object' }
+    }
+  );
 };
