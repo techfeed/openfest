9712d17e844b9dc8f7d2ea59f0efc577fa1fe557
dsuket <dai.takaoka@gmail.com>
2015-05-15 14:49:06 +0900

    update mailds on staging

--- server/datasources.staging.js
+++ server/datasources.staging.js
@@ -6,5 +6,14 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE || 'openfest'
+  },
+  emailDs: {
+    name: 'emailDs',
+    connector: 'mail',
+    transports: [
+      {
+        type: 'stub'
+      }
+    ]
   }
 };
